f00be2ee6a415e4ecd78c73e4b8f8d320980851f
Angela Lo <angelawenlo@gmail.com>
2019-06-10 18:59:27 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,6 +16,6 @@ require 'pry'
 
 def remove_strawberry(contacts)
   contacts.each do |contact, detail|
-    detail[:favorite_ice_cream_flavors].delete_if("strawberry")
+    detail(:favorite_ice_cream_flavors).delete_if("strawberry")
   end
 end
